904ec24b81a6908f7d5dfa7daa2d81f7e0034af0
nurjanahevs <evasitinurjanah2019@gmail.com>
2021-11-24 23:16:29 +0700

    i tried

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuccessComponent } from './success/success.component';
+import { InfoComponent } from './info/info.component';
+import { WarningComponent } from './warning/warning.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessComponent,
+    InfoComponent,
+    WarningComponent,
   ],
   imports: [
     BrowserModule,
